344f39517c135b342a6e860eae5c48b7163832f9
VenatioDecorus <opensourcekeys@gmail.com>
2018-04-17 21:01:57 -0400

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Client
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IPAddress ip = IPAddress.Parse("127.0.0.1");
+			int port = 1337;
+
+			TcpClient client = new TcpClient();
+			client.Connect(ip, port);
+			NetworkStream stream = client.GetStream();
+
+			Thread thread = new Thread(o => receiveData((TcpClient)o));
+			thread.Start(client);
+
+			string s;
+			while(!string.IsNullOrEmpty((s = Console.ReadLine())))
+			{
+				byte[] buffer = Encoding.ASCII.GetBytes(s);
+				stream.Write(buffer, 0, buffer.Length);
+			}
+			
+			client.Client.Shutdown(SocketShutdown.Send);
+			thread.Join();
+			stream.Close();
+			client.Close();
+		}
+
+		private static void receiveData(TcpClient client)
+		{
+			NetworkStream ns = client.GetStream();
+			byte[] receivedBytes = new byte[1014];
+			int byteCount;
+
+			while((byteCount = ns.Read(receivedBytes, 0, receivedBytes.Length)) > 0)
+			{
+				Console.WriteLine(Encoding.ASCII.GetString(receivedBytes, 0, byteCount));
+			}
+		}
+	}
+}
---
+++ ClientServer/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ClientServer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			TcpListener socket = new TcpListener(IPAddress.Any, 1337);
+			socket.Start();
+
+			while(true)
+			{
+				TcpClient client = socket.AcceptTcpClient();
+				Console.WriteLine("User connected");
+
+				Thread t = new Thread(handle_clients);
+				t.Start(client);
+			}
+		}
+
+		public static void handle_clients(object client)
+		{
+			TcpClient tcp = (TcpClient)client;
+
+			while (true)
+			{
+				NetworkStream stream = tcp.GetStream();
+				byte[] buffer = new byte[1024];
+				int byte_count = stream.Read(buffer, 0, buffer.Length);
+
+				if(byte_count == 0)
+				{
+					break;
+				}
+
+				writeClient(tcp, "Received data");
+
+				string data = Encoding.ASCII.GetString(buffer, 0, byte_count);
+				Console.WriteLine(data);
+			}
+
+			Console.WriteLine("Client disconnected");
+			tcp.Client.Shutdown(SocketShutdown.Both);
+			tcp.Close();
+		}
+
+		public static void writeClient(TcpClient c, string s)
+		{
+			byte[] buffer = Encoding.ASCII.GetBytes(s);
+			NetworkStream ns = c.GetStream();
+			ns.Write(buffer, 0, buffer.Length);
+		}
+	}
+}
